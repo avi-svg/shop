6f971bfee6f1b5ccdcc8394b1febddcb8af87274
avram <avramy.kunin@gmail.com>
2026-01-20 21:51:15 +0200

    Add Reviews component with styles and implement review display

---
+++ app/reviews/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import styles from "./page.module.css";
+import { useState } from "react";
+
+type Review = {
+  id: number
+  name: string;
+  review: string;
+};
+
+const Reviews = () => {
+  const [reviewsState, setReviewsState] = useState<Review[]>([
+    { id: 1, name: "דן", review: "אחלה מוצר" },
+    { id: 2, name: "נועה", review: "מאוד מרוצה" },
+    { id: 3, name: "David", review: "שווה את המחיר" },
+    { id: 4, name: "David", review: "שווה את המחיר" },
+    { id: 5, name: "David", review: "שווה את המחיר" },
+    { id: 6, name: "David", review: "שווה את המחיר" },
+    { id: 7, name: "David", review: "שווה את המחיר" },
+  ]);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Product Review</h1>
+      <div className={styles.reviewList}>
+        {reviewsState.map((review) => {
+          return (
+            <div key={review.id} className={styles.reviewCard}>
+              <h3 className={styles.reviewerName}>{review.name}</h3>
+              <p className={styles.reviewContant}>{review.review}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
