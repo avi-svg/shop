634faba15464cb1026dee3c5c5d70c20eaf3f9ec
avram <avramy.kunin@gmail.com>
2026-01-22 18:02:23 +0200

    Implement review submission functionality in Reviews component

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import styles from "./page.module.css";
-import { useState } from "react";
+import { BaseSyntheticEvent, useState } from "react";
 
 type Review = {
-  id: number
+  id?: number
   name: string;
   review: string;
 };
+
+
 
 const Reviews = () => {
   const [reviewsState, setReviewsState] = useState<Review[]>([
@@ -19,6 +21,40 @@ const Reviews = () => {
     { id: 6, name: "David", review: "שווה את המחיר" },
     { id: 7, name: "David", review: "שווה את המחיר" },
   ]);
+
+
+  const [newReview, setNewReview] = useState<Review>({
+    name: '',
+    review: ''
+  });
+
+
+  const handelNewReviewNameChang = (event: BaseSyntheticEvent) => {
+    const {name, value} = event.target;
+    
+    
+    setNewReview((prev) => {
+      return {...prev,
+        [name]: value
+      }
+    })
+  }
+
+  const handelSendReview = (event: BaseSyntheticEvent) =>{
+    event.preventDefault();
+
+    if(newReview.name && newReview.review){
+      setReviewsState((prev) => {
+        return(
+          [...prev, {...newReview}]
+        )
+      })
+      setNewReview({
+        name: '',
+        review: '',
+      })
+    }
+  }
 
   return (
     <div className={styles.container}>
@@ -33,15 +69,19 @@ const Reviews = () => {
           );
         })}
       </div>
-      <form className={styles.reviewForm}>
+      <form className={styles.reviewForm} onSubmit={handelSendReview}>
           <input type="text"
           name="name"
           placeholder="Your Name"
           className={styles.input}
+          value={newReview.name}
+          onChange={handelNewReviewNameChang}
           required />
-          <textarea name="content"
+          <textarea name="review"
           className={styles.textarea}
           placeholder="Write your review here..."
+          value={newReview.review}
+          onChange={handelNewReviewNameChang}
           required />
           <button
           type="submit"
